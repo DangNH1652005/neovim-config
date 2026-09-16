0add18bec7a5b707f3f98d553e0e6f34be1b3384
Nguyen Hai Dang <nguyenhaidang1652005@gmail.com>
2026-09-16 10:42:36 +0700

    config lsp for angular

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,8 +7,33 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      -- Lua
       vim.lsp.config("lua_ls", {
         cmd = { "lua-language-server" },
+        capabilities = capabilities,
+      })
+
+      -- Java
+      vim.lsp.config("jdtls", {
+        cmd = { "jdtls" },
+        capabilities = capabilities,
+      })
+
+      -- TypeScript / JavaScript
+      vim.lsp.config("ts_ls", {
+        cmd = { "typescript-language-server", "--stdio" },
+        capabilities = capabilities,
+      })
+
+      -- HTML
+      vim.lsp.config("html", {
+        cmd = { "vscode-html-language-server", "--stdio" },
+        capabilities = capabilities,
+      })
+
+      -- CSS
+      vim.lsp.config("cssls", {
+        cmd = { "vscode-css-language-server", "--stdio" },
         capabilities = capabilities,
       })
 
@@ -17,7 +42,7 @@ return {
         capabilities = capabilities,
       })
 
-      vim.lsp.enable({ "lua_ls", "jdtls" })
+      vim.lsp.enable({ "lua_ls", "jdtls", "ts_ls", "html", "cssls" })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover)
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
